926875170b09dcc112d0a5040f4fdec0c9afb705
nashbateman <nashbateman@yahoo.com>
2018-10-03 16:45:13 -0500

    Initial commit

--- RoombaAttack/Mob.gd
+++ RoombaAttack/Mob.gd
@@ -4,7 +4,7 @@ export (int) var min_speed # Minimum speed range.
 export (int) var max_speed # Maximum speed range.
 
 func _ready():
-	$Sprite.update()
+	$AnimatedSpriteprite.animate()
 	
 func _on_Visibility_screen_exited():
     queue_free()
