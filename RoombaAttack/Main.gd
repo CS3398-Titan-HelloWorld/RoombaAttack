c50076193bfa9ad3a49b06ad877eb3b688363638
Bkelley3 <brk28@txstate.edu>
2018-11-01 08:51:07 -0500

    Split the Main
    The Runtime script now handles all the timers and the start position

--- RoombaAttack/Main.gd
+++ RoombaAttack/Main.gd
@@ -13,29 +13,9 @@ func new_game():
 	$GUI.update_score(score)
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-func _on_StartTimer_timeout():
-    $MobTimer.start()
-    $ScoreTimer.start()
-func _on_ScoreTimer_timeout():
-	score += 1
-	$GUI.update_score(score)
-func _on_MobTimer_timeout():
-    # Choose a random location on Path2D.
-    $MobPath/MobSpawnLocation.set_offset(randi())
-    # Create a Mob instance and add it to the scene.
-    var mob = Mob.instance()
-    add_child(mob)
-    # Set the mob's direction perpendicular to the path direction.
-    var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
-    # Set the mob's position to a random location.
-    mob.position = $MobPath/MobSpawnLocation.position
-    # Add some randomness to the direction.
-    direction += rand_range(-PI / 4, PI / 4)
-    mob.rotation = direction
-    # Choose the velocity.
-    mob.set_linear_velocity(Vector2(rand_range(mob.min_speed, mob.max_speed), 0).rotated(direction))
 func _input(event):
 	if Input.is_action_pressed("ui_escape"):
 		get_tree().quit()
-func _on_EndPosition_body_entered(body):
-	game_over()
+#Not needed with the changing of levels
+#func _on_EndPosition_body_entered(body):
+	#game_over()
