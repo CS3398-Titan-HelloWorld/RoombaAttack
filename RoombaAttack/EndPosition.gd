df431c0a0ef0da7f4a93e9a4398f06608d9419e3
RamseyD <rsd30@txstate.edu>
2018-12-04 10:45:50 -0600

    Update to audio again

--- RoombaAttack/EndPosition.gd
+++ RoombaAttack/EndPosition.gd
@@ -4,9 +4,14 @@ signal hit
 
 export(String, FILE, "*.tscn") var next_level
 
+func _ready():
+	get_viewport().audio_listener_enable_2d = true
+	$crunch.play()
+	pass
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
-    
+			$crunch.play()
 			get_tree().change_scene(next_level)
