f2ef7233bf5ba9e1340f118362c8f6abfc6b70fb
nashbateman <33039086+nashbateman@users.noreply.github.com>
2018-12-04 13:49:09 -0600

    Add files via upload

--- RoombaAttack/EndPosition.gd
+++ RoombaAttack/EndPosition.gd
@@ -3,20 +3,11 @@ extends Area2D
 signal hit
 
 export(String, FILE, "*.tscn") var next_level
-<<<<<<< HEAD
-
-func _ready():
-	get_viewport().audio_listener_enable_2d = true
-	$crunch.play()
-	pass
-
-=======
 func _ready():
 	get_viewport().audio_listener_enable_2d = true
 	$AUDIO.play()
 	pass
 	
->>>>>>> 6c3e6003505bb1cb33a990c9b095cb79294c843c
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
