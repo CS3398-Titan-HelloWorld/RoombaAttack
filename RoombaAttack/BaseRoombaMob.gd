36fe96e6fcde2a32a4657af745afc90f5e6cc8b8
iantex <icc6@txstate.edu>
2018-12-04 01:04:18 -0600

    Integration of RoombaMob

--- RoombaAttack/BaseRoombaMob.gd
+++ RoombaAttack/BaseRoombaMob.gd
@@ -1,13 +1,20 @@
 ''' Author: Porter D. Chase '''
 ''' This class is for the base Roomba Mob. Will Extend the health class and then add collision detection signals '''
 
-extends "res://Scripts/EntityClass.gd"
+##extends "res://Scripts/EntityClass.gd"
+extends KinematicBody2D
 
 const TYPE = "Roomba"
-signal roomba_hit_obstacle
+signal roomba_hit_player
+export (int) var min_speed
+export (int) var max_speed
+export (float) var spawn_direction # the number multiplied by PI to get the radian
 
-''' This is currently useless, to be implemented later '''
+export (int) var damage
+export (int) var health
+
+var velocity = Vector2() 
 
 func CollisionDetection(body):
 	if body.Get("TYPE") != "Player":
-		emit("roomba_hit_obstacle")
+		emit("roomba_hit_player")
